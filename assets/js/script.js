e1369d80d6a516eddcb14965a3db4f7edc656b57
Jito Chadha <jitochadha@gmail.com>
2021-01-31 23:37:30 -0800

    updated with navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,30 @@
-// var userInput = $("#symbol");
+initializePage();
 
 
-
-const getNews = function (tickers, items) {
-
+function getNews(tickers, items) {
     fetch(`https://stocknewsapi.com/api/v1?tickers=${tickers.join()}&items=${items}&token=tvqftcxiwedbpjfxkixyqylbrjwvx3cjeoqmuvj8`)
         .then(response => response.json())
-        // .then(data => displayNewsList(data.data));
         .then(data => displayNewsList(data.data));
 };
-const getTickerImages = function (tickers) {
+
+function getTickerImages(tickers) {
     let items = 50;
     fetch(`https://stocknewsapi.com/api/v1?tickers=${tickers.join()}&items=${items}&token=tvqftcxiwedbpjfxkixyqylbrjwvx3cjeoqmuvj8`)
         // .then(function(response) {
         //     return response.json();
-        // })    
+        // })
+
         // .then(response => response.json())
         // .then(data => displayTickerImages(tickers, data.data));
-    // .then(data => {
-    //     return data.data.map(article => ({
-    //         tickers: article.tickers, 
-    //         imageURL: article.image_url
-    //     }));
-    // });
 };
+
 function displayTickerImages(tickers, newsList) {
     for (let ticker of tickers) {
         let imageUrl = getTickerImageUrl(ticker, newsList);
         displayTickerImage(ticker, imageUrl);
     }
 }
+
 function getTickerImageUrl(ticker, newsList) {
     const matchingStory = newsList.find(function (newsStory) {
         return newsStory.tickers.includes(ticker);
@@ -42,19 +37,11 @@ function getTickerImageUrl(ticker, newsList) {
     
     }
 };
+
 function displayTickerImage(ticker, imageUrl) {
     if(imageUrl === "") return;
     $(`#${ticker} img`).attr("src", imageUrl)
 }
-
-// function saveSymbol() {
-//     $("#symbol").on("click", $())
-// }
-// function fetchNews() {
-//     let fetchNewsButton = document.getElementById("button");
-//     fetchNewsButton.addEventListener("click", getNews);
-// }
-// fetchNews();
 
 function getTrending() {
     fetch("https://financialmodelingprep.com/api/v3/stock/actives?apikey=790c2982fd01273e3a03a32d42eb3273")
@@ -62,7 +49,6 @@ function getTrending() {
         .then(data => displayActiveStockList(data.mostActiveStock));
 
 };
-getTrending();
 
 function displayActiveStockList(activeStockList) {
     let tickers = activeStockList.map(stock => stock.ticker);
@@ -72,7 +58,6 @@ function displayActiveStockList(activeStockList) {
 };
 
 function displayStock(stock) {
-    // console.log(stock);
     let card = $(`<div id="${stock.ticker}"></div>`);
     card.addClass("card stock-card");
     card.click(() => handleStockClick(stock.ticker));
@@ -81,7 +66,6 @@ function displayStock(stock) {
     const companyName = $(`<div class="company-name"> <span>${stock.companyName}</span> </br> <span class="stock-symbol"> ${stock.ticker}</span> </div>`);
     const stockDetails = $(`<div class="stock-details"></div>`);
     const stockPrice = $(`<span class="stock-price"> ${stock.price}</span>`);
-    
     let stockChangesPercentage; 
     let percentChange = stock.changesPercentage.replace("%", "")
     percentChange = percentChange.replace("(", "")
@@ -91,40 +75,32 @@ function displayStock(stock) {
     } else {
         stockChangesPercentage = $(`<span class="stock-changes-percentage negative-change"> ${stock.changesPercentage}</span>`);
     };
-    
-    const stockSymbol = $(`<span class="stock-symbol"> ${stock.ticker}</span>`);
     const stockImage = $(`<img class="stock-image"></img>`);
-    
     divider.append(companyName);
     stockDetails.append(stockPrice);
     stockDetails.append(stockChangesPercentage);
-    // stockDetails.append(stockSymbol);
     divider.append(stockDetails);
     card.append(divider);
     card.append(stockImage);
     $("#results-list").append(card);
-    
-
-}
-function cardStyling() {
-    console.log($(".stock-changes-percentage"));
 }
 
 function handleStockClick(ticker) {
     // getNews(["gme", "tsla", "aapl"], 4);
     getNews([ticker], 5);
-
 }
 
 function displayNewsList(newsList) {
     clearNewsList();
     newsList.forEach(newsStory => displayStory(newsStory));
 }
+
 function displayStory(newsStory) {
     let listItem = $("<li></li>");
     listItem.html(`<a target="_blank" href="${newsStory.news_url}">${newsStory.title}</a>`);
     $("#news-list").append(listItem);
 }
+
 function handleFetchNewsSubmit(e) {
     e.preventDefault();
     let tickerInput = document.querySelector('#symbol');
@@ -132,8 +108,61 @@ function handleFetchNewsSubmit(e) {
     tickerInput.value = "";
 
 }
-$("#news-form").submit(handleFetchNewsSubmit)
 
 function clearNewsList() {
     $("#news-list").empty();
+}
+
+function initializePage() {
+    $("#news-form").submit(handleFetchNewsSubmit);
+    $(document).foundation();
+    getTrending();
+    $("#home-nav").click(handleHomeClick);
+    $("#trending-nav").click(handleTrendingClick);
+    $("#portfolio-nav").click(handlePortfolioClick);
+}
+
+function hideHome() {
+    $("#home").css("display", "none");
+}
+
+function hideTrending() {
+    $("#results-list").css("display", "none");
+}
+
+function hidePortfolio() {
+    $("#portfolio").css("display", "none");
+}
+
+function showHome() {
+    $("#home").css("display", "flex");
+}
+
+function showTrending() {
+    $("#results-list").css("display", "flex");
+}
+
+function showPortfolio() {
+    $("#portfolio").css("display", "flex");
+}
+
+function hideAllPages() {
+    hideHome();
+    hideTrending();
+    hidePortfolio();
+}
+
+function handleHomeClick() {
+    hideAllPages();
+    showHome();
+}
+
+function handleTrendingClick() {
+    hideAllPages();
+    showTrending();
+}
+
+function handlePortfolioClick() {
+    hideAllPages();
+    showPortfolio();
 }
