d5e50f9a33ac6bd223f501750f845ab2ee8cbab3
Toby <agentalliancela33@gmail.com>
2021-02-01 20:07:26 -0800

    fixed merge conflict

---
+++ assets/js/cryptoapi1.js
@@ -0,0 +1,21 @@
+// function getCoinInfo (){
+//     console.log(working);
+//     fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false')
+//     .then(response => response.json())
+//     .then(data => (data.data));
+    
+// };
+// getCoinInfo();
+
+function getCoinInfo() {
+    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false')
+    .then(function(response){
+      return response.json();
+    }).then(function(data) {
+      console.log(data);
+    })
+};
+
+// function getTickerImages(tickers) {
+//     let items = 50;
+//     fetch(`https://stocknewsapi.com/api/v1?tickers=${tickers.join()}&items=${items}&token=tvqftcxiwedbpjfxkixyqylbrjwvx3cjeoqmuvj8`)
